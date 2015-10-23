3743345798ea453027a97d3de97ab5aa9fd6ffd5
Gennady Glinsky <gesha@maytech.net>
2015-10-23 13:00:38 +0300

    Added events for graceful workers exit

--- producer-consumers/main.py
+++ producer-consumers/main.py
@@ -1,17 +1,38 @@
 import logging
 import os
 import argparse
+import threading
 from time import time
 from Queue import Queue
 from threading import Thread
 from os import SEEK_SET
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
+class ProcessThread(Thread):
+    def __init__(self, line_queue, print_queue):
+        super(ProcessThread, self).__init__()
+        self.lines_queue = line_queue
+        self.print_queue = print_queue
+        self.stoprequest = threading.Event()
+
+    def run(self):
+        #logger.info(self.stoprequest.isSet() and self.line_queue.empty())
+        #logger.info(self.line_queue.empty())
+        while not (self.stoprequest.isSet() and self.lines_queue.empty()):
+            # Get the work from the queue
+            line = self.lines_queue.get()
+            logger.debug('Processing:"{}"'.format(line))
+            if line[0].isupper():
+                self.print_queue.put(line)
+            self.lines_queue.task_done()
+
+
 def process_lines(lines_queue, print_queue):
-    while True:
+    stoprequest = threading.Event()
+    while not stoprequest and not lines_queue.empty():
         # Get the work from the queue
         line = lines_queue.get()
         logger.debug('Processing:"{}"'.format(line))
@@ -47,13 +68,17 @@ def main(infile, outfile):
     lines_queue = Queue()
     print_queue = Queue()
 
+    workers = []
+
 
     # Create worker threads
     for x in range(4):
-        worker = Thread(target=process_lines, args=(lines_queue, print_queue))
+        #worker = Thread(target=process_lines, args=(lines_queue, print_queue))
+        worker = ProcessThread(lines_queue, print_queue)
         # Setting daemon to True will let the main thread exit even though the workers are blocking
         worker.daemon = True
         worker.start()
+        workers.append(worker)
 
     # Spawn threads to write
     writer = Thread(target=write_lines, args=(print_queue, outfile))
@@ -64,6 +89,9 @@ def main(infile, outfile):
     for line in read_lines(infile):
         logger.debug('Queueing:"{}"'.format(line))
         lines_queue.put(line)
+
+    for worker in workers:
+        worker.stoprequest.set()
 
     # Causes the main thread to wait for the queue to finish processing all the tasks
     print_queue.join()
