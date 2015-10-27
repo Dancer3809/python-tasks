dcc9737d55e4f86cf7a5eb6e20c356a01b777fba
Gennady Glinsky <gesha@maytech.net>
2015-10-27 10:01:44 +0200

    Added coroutine for writing lines

--- producer-consumers/main2.py
+++ producer-consumers/main2.py
@@ -1,21 +1,32 @@
 import logging
 import argparse
-from time import time
+from time import time, sleep
 from multiprocessing.pool import ThreadPool
 from multiprocessing import Pool
-#import os
-#from Queue import Empty
-#from threading import Thread
-#from multiprocessing import Process, JoinableQueue, Event, Lock, Manager, Pool
-#from os import SEEK_SET
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(process)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 def process(line):
+    #sleep(0.01)
     if line[0].isupper():
         return line
+
+def coroutine(func):
+    def start(*args,**kwargs):
+        cr = func(*args,**kwargs)
+        cr.next()
+        return cr
+    return start
+
+@coroutine
+def write_line(outfile):
+    while True:
+        lines = yield
+        for line in lines:
+            if line:
+                outfile.write(line)
 
 
 def main(infile, outfile, run_processes, workers_count=4):
@@ -27,13 +38,10 @@ def main(infile, outfile, run_processes, workers_count=4):
         logger.info("Start threads")
         pool = ThreadPool(processes=workers_count)
 
-    def write_line(lines):
-        for line in lines:
-            if line:
-                outfile.write(line)
+    writer = write_line(outfile)
 
     logger.info("Start line processing")
-    pool.map_async(process, infile, callback=write_line)
+    pool.map_async(process, infile, callback=lambda line: writer.send(line))
 
     logger.info("Close pool")
     pool.close()
@@ -47,7 +55,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=argparse.FileType('r'))
     parser.add_argument('outfile', type=argparse.FileType('w'))
-    #parser.add_argument('outfile')
     parser.add_argument('-p', '--process', action='store_true', help='fork processes instead of threads')
     parser.add_argument('-c', '--concurent', nargs='?', type=int, const=4, default=4, help='number of threads (processes)')
     args = parser.parse_args()
